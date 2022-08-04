de5ce6e1422fe0b6bc4019553a77dedee849b3b7
Mariano Ravinale <mravinale@gmail.com>
2022-08-04 13:06:24 -0300

    initial setup

---
+++ src/app/user.ts
@@ -0,0 +1,17 @@
+
+export class User {
+
+    private _name!: string;
+
+    constructor(name:string) {
+        this.name = name;
+    }
+
+    get name(): string {
+        return this._name;
+    }
+
+    set name(name: string) {
+        this._name = name;
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { User } from "./app/user";
+
+const user = new User("John");
+
+console.log(`User Name: ${user.name}`)
